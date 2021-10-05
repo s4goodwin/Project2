168d8c575163d6a0e6ccc5ea3917357fc5ed7049
s4goodwin <90581608+s4goodwin@users.noreply.github.com>
2021-10-05 17:51:40 -0400

    10/5 5:51

--- src/com/practice/Store.java
+++ src/com/practice/Store.java
@@ -1,29 +1,31 @@
 package com.practice;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Store {
-    private ArrayList <Order> Orders;
-    private ArrayList <Customer> Customers;
+    private ArrayList<Order> Orders;
+    private ArrayList<Customer> Customers;
+    private Scanner scanner;
 
-    public Store (){
+    public Store(String[] args) throws FileNotFoundException {
+        scanner = new Scanner(new File(""));
         Orders = new ArrayList<Order>();
         Customers = new ArrayList<Customer>();
-        var filename = "Customers.txt";
-        Scanner scanner = new Scanner();
+        List<String> list = Collections.emptyList();
 
+        try {
+            list = Files.readAllLines(Paths.get("Customers.txt"));
+            String[]names=list.get(0).split(",");
+
+        }
 
     }
-//    public static void Store(String [] args) throws FileNotFoundException {
-//
-//
-//        try{
-//            Scanner scanner = new Scanner(new FileReader("Customers.txt"));
-//        }
-//
-//
-//    }
 }
