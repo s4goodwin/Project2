46d16ffe897e5b0d4aced183f8da288f6b080a87
s4goodwin <90581608+s4goodwin@users.noreply.github.com>
2021-10-05 23:50:11 -0400

    10/5 11:50

--- src/com/practice/Store.java
+++ src/com/practice/Store.java
@@ -2,34 +2,24 @@ package com.practice;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
-import java.util.function.Consumer;
 
 public class Store {
     private ArrayList<Order> Orders;
-    private ArrayList<Customer> Customers;
+    private ArrayList<String> Customers;
     private Scanner scanner;
 
     public List<String> Store(String[] args) throws FileNotFoundException {
-        scanner = new Scanner(new File(""));
+        //scanner = new Scanner(new File(""));
         Orders = new ArrayList<Order>();
-        Customers = new ArrayList<Customer>();
+        Customers = new ArrayList<String>();
         List<String> lines = Collections.emptyList();
 
-        try {
-            lines = Files.readAllLines(Paths.get("Customers.txt"), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
+        try (Scanner scanner = new Scanner(new File("Customers.txt")).useDelimiter("\\s*,\\s*")){
+            while (scanner.hasNext()){
+                Customers.add(scanner.next());
+            }
         }
-        String[]names = lines.get(0).split(",");
-//        Consumer<? super String> customerName;
-//        Arrays.asList(names).forEach(customerName);{
-//            Customers.add(new Customer(names));
-//            System.out.println(Customers);
-//        }
+    return Customers;
     }
 }
