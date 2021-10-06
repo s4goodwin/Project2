9c86a32d0843890ce1d650e1a06edf660397cee1
s4goodwin <90581608+s4goodwin@users.noreply.github.com>
2021-10-05 22:59:31 -0400

    10/5 11:00

--- src/com/practice/Order.java
+++ src/com/practice/Order.java
@@ -12,8 +12,15 @@ public class Order {
     public String getDestination(){
         return destination.toString();
     }
+
     public String getOrderedBy(){
         return orderedBy.toString();
+    }
+
+    public String OrderDest () {
+        var OrderDest = orderedBy+"\n"+destination;
+        String OrderDest1 = OrderDest;
+        return OrderDest1.toString();
     }
 }
 
